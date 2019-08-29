f7691dc6eb596b7e3c7a26eac8a6156f70b9d6e8
Steven Riley <rileystevend@gmail.com>
2019-08-29 18:09:44 -0500

    add ant home layout

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css'
+const { Content, Footer, Sider, Header } = Layout;
+
+function App() {
+  return (
+  <div>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider>Sider</Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
+  </div>
+  );
+}
+
+export default App;
