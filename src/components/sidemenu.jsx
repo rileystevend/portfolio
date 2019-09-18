da10c8ba0b14150983cf064cfc9fb4acbe038888
Steven Riley <rileystevend@gmail.com>
2019-09-18 14:17:50 -0500

    added sidemenu notes

--- src/components/sidemenu.jsx
+++ src/components/sidemenu.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+//import menu from antd?
 import 'antd/dist/antd.css'
 
 function SideMenu () {
   return (
      <div>
-         
+{/* add antd menu here? */}
      </div> 
   );
   
