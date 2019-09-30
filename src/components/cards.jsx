8d6721396706e3664e4f6ab3e959fb0eba5b1d10
Steven Riley <rileystevend@gmail.com>
2019-09-30 15:16:01 -0500

    card render

---
+++ src/components/cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card } from 'antd';
+import 'antd/dist/antd.css'
+
+function Cards () {
+  return (
+  <Card>
+
+    </Card>
+  );
+
+}
+
+
+
+export default Cards;
