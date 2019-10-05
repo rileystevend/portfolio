262e8671c7ed877db6d91ba16f48e6f6dedd31aa
Steven Riley <rileystevend@gmail.com>
2019-10-04 22:46:12 -0500

    added multiple cards

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -26,6 +26,46 @@ function Cards () {
       description="This is the description"
     />
   </Card>
+  <Card
+    style={{ width: 300 }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <Icon type="setting" key="setting" />,
+      <Icon type="edit" key="edit" />,
+      <Icon type="ellipsis" key="ellipsis" />,
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
+  <Card
+    style={{ width: 300 }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <Icon type="setting" key="setting" />,
+      <Icon type="edit" key="edit" />,
+      <Icon type="ellipsis" key="ellipsis" />,
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
   );
 
 }
